adf97d1b9aca8dbe0796a85d9ea0079efda77930
Blago24 <blago102@gmail.com>
2017-01-12 21:15:35 +0200

    functions in chronological row

--- src/Playfair.java
+++ src/Playfair.java
@@ -18,45 +18,13 @@ public class Playfair {
 	private static final int START_INDEX_1 = 1;
 
 	public static void main(String[] args) {
-		
+
 		char[] key = insertingKey();
 		char[][] table = makingTheTableFromTheKey(key);
 		showTable(table);
 		char[] text = insertingText();
 		System.out.println(Arrays.toString(text));
 		showTheEncriptedText(table, text);
-	}
-	
-
-	private static void showTheEncriptedText(char[][] table, char[] text) {
-		char[] encriptedText = new char[2];
-		char[] ecncriptedTextForWindow = new char[text.length];
-		for (int row = START_INDEX_1; row < text.length; row += 2) {
-			char first = text[row - START_INDEX_1];
-			char second = text[row];
-			int[] indexesFromRowsAndCols = findingTheIndexesOfTheLettersInTheTable(first, second, table);
-			int firstLetterRow = indexesFromRowsAndCols[FIRST_LETTER_ROW];
-			int firstLetterCol = indexesFromRowsAndCols[FIRST_LETTER_COL];
-			int secondLetterRow = indexesFromRowsAndCols[SECOND_LETTER_ROW];
-			int secondLetterCol = indexesFromRowsAndCols[SECOND_LETTER_COL];
-			// System.out.println(Arrays.toString(indexesFromRowsAndCols));
-			encriptedText = encoding(firstLetterRow, firstLetterCol, secondLetterRow, secondLetterCol, table);
-			ecncriptedTextForWindow[row - START_INDEX_1] = encriptedText[START_INDEX_0];
-			ecncriptedTextForWindow[row] = encriptedText[START_INDEX_1];
-		}
-		for (int i = START_INDEX_1; i < ecncriptedTextForWindow.length; i += 2) {
-			System.out.print(ecncriptedTextForWindow[i - START_INDEX_1] + "" + ecncriptedTextForWindow[i] + " ");
-		}
-
-	}
-
-	private static void showTable(char[][] table) {
-		for (int rows = START_INDEX_0; rows < TABLE_SIZE; rows++) {
-			for (int cols = START_INDEX_0; cols < TABLE_SIZE; cols++) {
-				System.out.print(table[rows][cols] + " ");
-			}
-			System.out.println();
-		}
 	}
 
 	private static char[] insertingKey() {
@@ -65,18 +33,7 @@ public class Playfair {
 		String keyString = scanner.nextLine();
 		char[] arrayWithTheKeyBeforeTheCut = keyString.toCharArray();
 		char[] key = makingTheKey(arrayWithTheKeyBeforeTheCut);
-		
 		return key;
-	}
-
-	private static char[] insertingText() {
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("Put the text:");
-		String textString = scanner.nextLine();
-		char[] arrayWithTheTextBeforeTheCut = textString.toCharArray();
-		char[] text = makingTheText(arrayWithTheTextBeforeTheCut);
-		
-		return text;
 	}
 
 	private static char[] makingTheKey(char[] arrayWithTheKeyBeforeTheCut) {
@@ -87,6 +44,25 @@ public class Playfair {
 		char[] finalArrayWithKey = removingTheSpaces(arrayWithTheKeyBeforeTheCut);
 		return becomeUpperLetters(finalArrayWithKey);
 		// System.out.println(Arrays.toString(finalArrayWithKey));
+	}
+
+	private static char[] removingTheSpaces(char[] arrayWithTheTextBeforeTheCut) {
+		char[] arrayWithTheText = new char[arrayWithTheTextBeforeTheCut.length];
+		byte countForTheIndexForTheTextWithoutTheSpaces = 0;
+		for (int row = START_INDEX_0; row < arrayWithTheTextBeforeTheCut.length; row++) {
+			if (arrayWithTheTextBeforeTheCut[row] != ' ') {
+				arrayWithTheText[countForTheIndexForTheTextWithoutTheSpaces] = arrayWithTheTextBeforeTheCut[row];
+				countForTheIndexForTheTextWithoutTheSpaces++;
+			} else {
+				continue;
+			}
+		}
+		// System.out.println(Arrays.toString(arrayWithTheKey));
+		char[] finalArrayWithText = new char[countForTheIndexForTheTextWithoutTheSpaces];
+		for (int row = START_INDEX_0; row < countForTheIndexForTheTextWithoutTheSpaces; row++) {
+			finalArrayWithText[row] = arrayWithTheText[row];
+		}
+		return finalArrayWithText;
 	}
 
 	private static char[] becomeUpperLetters(char[] finalArrayWithKey) {
@@ -109,7 +85,56 @@ public class Playfair {
 				theArrayWithTheTableOneDimensional);
 
 		return theArrayWithTheTable;
+	}
 
+	private static int makingTheTableToTheKeyLength(char[] theArrayWithTheKey,
+			char[] theArrayWithTheTableOneDimensional) {
+		int countForTheDifferentLettersWhichPass = 0;
+		int countForTheDifferentLetters;
+		for (int rows = START_INDEX_0; rows < theArrayWithTheKey.length; rows++) {
+			countForTheDifferentLetters = 0;
+			for (int cols = START_INDEX_0; cols < theArrayWithTheKey.length; cols++) {
+				if (theArrayWithTheKey[rows] != theArrayWithTheTableOneDimensional[cols]) {
+					countForTheDifferentLetters++;
+				}
+				if (countForTheDifferentLetters == theArrayWithTheKey.length) {
+					if (theArrayWithTheKey[rows] == THE_J) {
+						theArrayWithTheTableOneDimensional[countForTheDifferentLettersWhichPass] = THE_I;
+						countForTheDifferentLettersWhichPass++;
+					} else {
+						theArrayWithTheTableOneDimensional[countForTheDifferentLettersWhichPass] = theArrayWithTheKey[rows];
+						countForTheDifferentLettersWhichPass++;
+					}
+				}
+			}
+		}
+		return countForTheDifferentLettersWhichPass;
+	}
+
+	private static char makingTheTableAfterTheKeyLength(int countForTheDifferentLettersWhichPass,
+			char[] theArrayWithTheTableOneDimensional) {
+		char theLetterWithTheSmallestPosition = 'A';
+		int countForTheDifferentLetters;
+		for (int rows = countForTheDifferentLettersWhichPass; rows < ONE_DIMENSIONAL_ARRAY_SIZE; rows++) {
+			countForTheDifferentLetters = 0;
+			for (int cols = START_INDEX_0; cols < ONE_DIMENSIONAL_ARRAY_SIZE; cols++) {
+				if (theLetterWithTheSmallestPosition != theArrayWithTheTableOneDimensional[cols]) {
+					countForTheDifferentLetters++;
+				}
+			}
+			if (countForTheDifferentLetters == ONE_DIMENSIONAL_ARRAY_SIZE) {
+				theArrayWithTheTableOneDimensional[rows] = theLetterWithTheSmallestPosition;
+
+			} else if (countForTheDifferentLetters == ONE_DIMENSIONAL_ARRAY_SIZE - 1) {
+				rows--;
+			}
+			if (theLetterWithTheSmallestPosition != THE_I) {
+				theLetterWithTheSmallestPosition++;
+			} else {
+				theLetterWithTheSmallestPosition += 2;
+			}
+		}
+		return theLetterWithTheSmallestPosition;
 	}
 
 	private static char[][] changeTheTableFromOneDimensionalArrayToTwoDimensional(
@@ -125,62 +150,14 @@ public class Playfair {
 		return theArrayWithTheTable;
 	}
 
-	private static char makingTheTableAfterTheKeyLength(int countForTheDifferentLettersWhichPass,
-			char[] theArrayWithTheTableOneDimensional) {
-		char theLetterWithTheSmallestPosition = 'A';
-		int countForTheDifferentLetters;
-		for (int rows = countForTheDifferentLettersWhichPass; rows < ONE_DIMENSIONAL_ARRAY_SIZE; rows++) {
-			countForTheDifferentLetters = 0;
-			for (int cols = START_INDEX_0; cols < ONE_DIMENSIONAL_ARRAY_SIZE; cols++) {
-				if (theLetterWithTheSmallestPosition != theArrayWithTheTableOneDimensional[cols]) {
+	private static char[] insertingText() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Put the text:");
+		String textString = scanner.nextLine();
+		char[] arrayWithTheTextBeforeTheCut = textString.toCharArray();
+		char[] text = makingTheText(arrayWithTheTextBeforeTheCut);
 
-					countForTheDifferentLetters++;
-
-				}
-
-			}
-
-			if (countForTheDifferentLetters == ONE_DIMENSIONAL_ARRAY_SIZE) {
-				theArrayWithTheTableOneDimensional[rows] = theLetterWithTheSmallestPosition;
-
-			} else if (countForTheDifferentLetters == ONE_DIMENSIONAL_ARRAY_SIZE - 1) {
-				rows--;
-			}
-			if (theLetterWithTheSmallestPosition != THE_I) {
-				theLetterWithTheSmallestPosition++;
-			} else {
-				theLetterWithTheSmallestPosition += 2;
-			}
-
-		}
-		return theLetterWithTheSmallestPosition;
-	}
-
-	private static int makingTheTableToTheKeyLength(char[] theArrayWithTheKey,
-			char[] theArrayWithTheTableOneDimensional) {
-		int countForTheDifferentLettersWhichPass = 0;
-		int countForTheDifferentLetters;
-		for (int rows = START_INDEX_0; rows < theArrayWithTheKey.length; rows++) {
-			countForTheDifferentLetters = 0;
-			for (int cols = START_INDEX_0; cols < theArrayWithTheKey.length; cols++) {
-
-				if (theArrayWithTheKey[rows] != theArrayWithTheTableOneDimensional[cols]) {
-					countForTheDifferentLetters++;
-
-				}
-				if (countForTheDifferentLetters == theArrayWithTheKey.length) {
-					if (theArrayWithTheKey[rows] == THE_J) {
-						theArrayWithTheTableOneDimensional[countForTheDifferentLettersWhichPass] = THE_I;
-						countForTheDifferentLettersWhichPass++;
-					} else {
-						theArrayWithTheTableOneDimensional[countForTheDifferentLettersWhichPass] = theArrayWithTheKey[rows];
-						countForTheDifferentLettersWhichPass++;
-					}
-				}
-			}
-
-		}
-		return countForTheDifferentLettersWhichPass;
+		return text;
 	}
 
 	private static char[] makingTheText(char[] arrayWithTheTextBeforeTheCut) {
@@ -224,25 +201,32 @@ public class Playfair {
 		return array;
 	}
 
-	private static char[] removingTheSpaces(char[] arrayWithTheTextBeforeTheCut) {
-		char[] arrayWithTheText = new char[arrayWithTheTextBeforeTheCut.length];
+	private static int[] findingTheIndexesOfTheLettersInTheTable(char firstLetter, char secondLetter, char[][] table) {
 
-		byte countForTheIndexForTheTextWithoutTheSpaces = 0;
-		for (int row = START_INDEX_0; row < arrayWithTheTextBeforeTheCut.length; row++) {
-			if (arrayWithTheTextBeforeTheCut[row] != ' ') {
-				arrayWithTheText[countForTheIndexForTheTextWithoutTheSpaces] = arrayWithTheTextBeforeTheCut[row];
-				countForTheIndexForTheTextWithoutTheSpaces++;
-			} else {
-				continue;
+		byte indexRowFirstLetter = START_INDEX_0;
+		byte indexColFirstLetter = START_INDEX_0;
+		byte indexRowSecondLetter = START_INDEX_0;
+		byte indexColSecondLetter = START_INDEX_0;
+		int[] indexesFromRowsAndCols = new int[4];
+		for (int rows = START_INDEX_0; rows < table.length; rows++) {
+			for (int cols = START_INDEX_0; cols < table.length; cols++) {
+				if (firstLetter == table[rows][cols]) {
+					indexRowFirstLetter = (byte) rows;
+					indexColFirstLetter = (byte) cols;
+				}
+				if (secondLetter == table[rows][cols]) {
+					indexRowSecondLetter = (byte) rows;
+					indexColSecondLetter = (byte) cols;
+				}
 			}
+		}
 
-		}
-		// System.out.println(Arrays.toString(arrayWithTheKey));
-		char[] finalArrayWithText = new char[countForTheIndexForTheTextWithoutTheSpaces];
-		for (int row = START_INDEX_0; row < countForTheIndexForTheTextWithoutTheSpaces; row++) {
-			finalArrayWithText[row] = arrayWithTheText[row];
-		}
-		return finalArrayWithText;
+		indexesFromRowsAndCols[FIRST_LETTER_ROW] = indexRowFirstLetter;
+		indexesFromRowsAndCols[FIRST_LETTER_COL] = indexColFirstLetter;
+		indexesFromRowsAndCols[SECOND_LETTER_ROW] = indexRowSecondLetter;
+		indexesFromRowsAndCols[SECOND_LETTER_COL] = indexColSecondLetter;
+
+		return indexesFromRowsAndCols;
 	}
 
 	private static char[] encoding(int firstLetterRow, int firstLetterCol, int secondLetterRow, int secondLetterCol,
@@ -303,32 +287,35 @@ public class Playfair {
 
 	}
 
-	private static int[] findingTheIndexesOfTheLettersInTheTable(char firstLetter, char secondLetter, char[][] table) {
+	private static void showTheEncriptedText(char[][] table, char[] text) {
+		char[] encriptedText = new char[2];
+		char[] ecncriptedTextForWindow = new char[text.length];
+		for (int row = START_INDEX_1; row < text.length; row += 2) {
+			char first = text[row - START_INDEX_1];
+			char second = text[row];
+			int[] indexesFromRowsAndCols = findingTheIndexesOfTheLettersInTheTable(first, second, table);
 
-		byte indexRowFirstLetter = START_INDEX_0;
-		byte indexColFirstLetter = START_INDEX_0;
-		byte indexRowSecondLetter = START_INDEX_0;
-		byte indexColSecondLetter = START_INDEX_0;
-		int[] indexesFromRowsAndCols = new int[4];
-		for (int rows = START_INDEX_0; rows < table.length; rows++) {
-			for (int cols = START_INDEX_0; cols < table.length; cols++) {
-				if (firstLetter == table[rows][cols]) {
-					indexRowFirstLetter = (byte) rows;
-					indexColFirstLetter = (byte) cols;
-				}
-				if (secondLetter == table[rows][cols]) {
-					indexRowSecondLetter = (byte) rows;
-					indexColSecondLetter = (byte) cols;
-				}
-			}
+			int firstLetterRow = indexesFromRowsAndCols[FIRST_LETTER_ROW];
+			int firstLetterCol = indexesFromRowsAndCols[FIRST_LETTER_COL];
+			int secondLetterRow = indexesFromRowsAndCols[SECOND_LETTER_ROW];
+			int secondLetterCol = indexesFromRowsAndCols[SECOND_LETTER_COL];
+
+			// System.out.println(Arrays.toString(indexesFromRowsAndCols));
+			encriptedText = encoding(firstLetterRow, firstLetterCol, secondLetterRow, secondLetterCol, table);
+			ecncriptedTextForWindow[row - START_INDEX_1] = encriptedText[START_INDEX_0];
+			ecncriptedTextForWindow[row] = encriptedText[START_INDEX_1];
 		}
-
-		indexesFromRowsAndCols[FIRST_LETTER_ROW] = indexRowFirstLetter;
-		indexesFromRowsAndCols[FIRST_LETTER_COL] = indexColFirstLetter;
-		indexesFromRowsAndCols[SECOND_LETTER_ROW] = indexRowSecondLetter;
-		indexesFromRowsAndCols[SECOND_LETTER_COL] = indexColSecondLetter;
-
-		return indexesFromRowsAndCols;
+		for (int i = START_INDEX_1; i < ecncriptedTextForWindow.length; i += 2) {
+			System.out.print(ecncriptedTextForWindow[i - START_INDEX_1] + "" + ecncriptedTextForWindow[i] + " ");
+		}
 	}
 
+	private static void showTable(char[][] table) {
+		for (int rows = START_INDEX_0; rows < TABLE_SIZE; rows++) {
+			for (int cols = START_INDEX_0; cols < TABLE_SIZE; cols++) {
+				System.out.print(table[rows][cols] + " ");
+			}
+			System.out.println();
+		}
+	}
 }
